a6736549d70c45cad15fe26455a31bdac6ddc358
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2018-03-05 15:42:02 -0800

    added in a Dockerfile
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

---
+++ run.cmd
@@ -0,0 +1,8 @@
+docker stop fabric.machinelearning.basos
+docker rm fabric.machinelearning.baseos
+
+docker pull healthcatalyst/fabric.docker.centos.tomcat:latest
+
+docker build -t healthcatalyst/fabric.machinelearning.baseos . 
+# docker run -p 8085:8085 --rm --name machinelearning -t healthcatalyst/fabric.machinelearning.baseos
+docker run -p 8085:8085 -it --name machinelearning -t healthcatalyst/fabric.machinelearning.baseos --entrypoint=/bin/bash
